f6a54e6d4896485147efafc07ea315f831af099c
rockn1989 <gold_100@bk.ru>
2023-05-15 17:02:03 +0300

    fix modal

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -1,29 +1,29 @@
-// import { ModalProps } from './Modal.props';
+import { ModalProps } from './Modal.props';
 
-// import cn from 'classnames';
-// import styles from './Modal.module.scss';
-// import React, { forwardRef } from 'react';
+import cn from 'classnames';
+import styles from './Modal.module.scss';
+import React, { forwardRef } from 'react';
 
-// export const Modal = forwardRef<HTMLDivElement, ModalProps>(({ className, onClickForClose, children, ...rest }, ref) => {
+export const Modal = forwardRef<HTMLDivElement, ModalProps>(({ className, onClickForClose, children, ...rest }, ref) => {
 
-//   const handleModalClose = (event: React.MouseEvent): void => {
-//     if (event.target === ref?.current) {
-//       onClickForClose();
-//     }
-//   }
+  // const handleModalClose = (event: React.MouseEvent): void => {
+  //   if (event.target === ref?.current) {
+  //     onClickForClose();
+  //   }
+  // }
 
-//   return (
-//     <div
-//       ref={ref}
-//       className={styles.overlay}
-//       onClick={(event) => handleModalClose(event)}
-//       {...rest}
-//     >
-//       <div className={cn(className, styles.container)}>
-//         {children}
-//       </div>
-//     </div>
-//   )
-// });
+  return (
+    <div
+      ref={ref}
+      className={styles.overlay}
+      //onClick={(event) => handleModalClose(event)}
+      {...rest}
+    >
+      <div className={cn(className, styles.container)}>
+        {children}
+      </div>
+    </div>
+  )
+});
 
-// Modal.displayName = 'Modal';
+Modal.displayName = 'Modal';
